1212070070c5e6c4938d74bc1ef9fbfdfd3b2fb7
Work <rewinell.guardiano@students.ittralee.ie>
2025-01-21 20:39:34 +0000

    Signed-off-by: Work <rewinell.guardiano@students.ittralee.ie>

---
+++ GMBTouchAssignment/Assets/ITouchable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface ITouchable
+{
+    void SelectToggle(bool isSelected);
+}
---
+++ GMBTouchAssignment/Assets/ManagerScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerScript : MonoBehaviour
+{
+    ITouchable selectedObject;
+    void Start()
+    {
+    }
+    void Update()
+    {
+
+    }
+    public void tapRegisteredAt(Vector2 tapPosition)
+    {
+        Ray r = Camera.main.ScreenPointToRay(tapPosition);
+        RaycastHit info;
+        if (Physics.Raycast(r, out info))
+        {
+            ITouchable newObject = info.collider.gameObject.GetComponent<ITouchable>();
+            if (newObject != null)
+            {
+                if (selectedObject != null)
+                {
+                    selectedObject.SelectToggle(false);
+                }
+                selectedObject = newObject;
+                newObject.SelectToggle(true);
+
+            }
+        }
+    }
+}
